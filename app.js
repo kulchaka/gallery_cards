d48e7d69bd76e76e0618b7c72aec1983a776e90b
Andrii <kulchaka@gmail.com>
2021-06-17 09:15:43 +0200

    clear active slide

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const slides = document.querySelectorAll('.slide')
 
 slides.forEach(e => {
   e.addEventListener('click', () => {
+    clearActiveSlide()
     e.classList.add('active')
   })
 })
+
+function clearActiveSlide() {
+  slides.forEach(e => {
+    e.classList.remove('active')
+  })
+}
